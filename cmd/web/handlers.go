a5e7348d2d8a870580502bf046531aac16b7ce21
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-06-30 11:38:56 +0700

    using template for dynamic html

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,12 +13,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
-	// File name is relative to current working directory ( the one running cmd )
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	files := []string{
+		"./ui/html/base.tmpl.gohtml", // this must be the 1st file in the slice
+		"./ui/html/pages/home.tmpl.gohtml",
+	}
+	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
 		log.Print(err.Error())
@@ -26,11 +25,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// We then use the Execute() method on the template set to write the
-	// template content as the response body. The last parameter to Execute()
-	// represents any dynamic data that we want to pass in, which for now we'll
-	// leave as nil.
-	err = ts.Execute(w, nil)
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
