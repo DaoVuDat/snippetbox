0df8b388c6e20e9fc2397f4a03d48f1bd3b76eb3
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-07-02 18:50:17 +0700

    add mysql connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -15,14 +17,26 @@ type application struct {
 func main() {
 	// Add flag for command line
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	// Define a new command-line flag for the MySQL DSN string.
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Create custom logger
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// Initialize a new instance of our application struct, containing the
-	// dependencies.
+	// To keep the main() function tidy I've put the code for creating a connection
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag.
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
@@ -40,7 +54,26 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 
+}
+
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// The sql.Open() function doesn't actually create any connections,
+	// all it does is initialize the pool for future use.
+	// Actual connections to the database are established lazily,
+	// as and when needed for the first time.
+	// So to verify that everything is set up correctly
+	// we need to use the db.Ping() method to create a connection and check for any errors.
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
