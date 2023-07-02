fa454b561638b49f714da54fa550f5a689ad263b
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-07-02 17:34:40 +0700

    isolate the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,17 +28,6 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Create Router
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
 	// that the server uses the same network address and routes as before, and set
 	// the ErrorLog field so that the server now uses the custom errorLog logger in
@@ -46,7 +35,8 @@ func main() {
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		// Call the new app.routes() method to get the servemux containing our routes.
+		Handler: app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
