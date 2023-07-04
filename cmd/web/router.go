32a8e7eccf616fd5577aaab6b44ad38d2320b762
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-07-04 12:45:26 +0700

    add middleware

--- cmd/web/router.go
+++ cmd/web/router.go
@@ -2,8 +2,9 @@ package main
 
 import "net/http"
 
-// The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+// Update the signature for the routes() method so that it returns a
+// http.Handler instead of *http.ServeMux.
+func (app *application) routes() http.Handler {
 	// Create Router
 	mux := http.NewServeMux()
 
@@ -15,5 +16,8 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
+	// Because secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	return secureHeaders(mux)
 }
