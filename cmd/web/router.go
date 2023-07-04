c8791cb24806985a5515c97133dafb964889de07
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-07-04 15:20:50 +0700

    using justinas/alice for composing middleware chain

--- cmd/web/router.go
+++ cmd/web/router.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 // Update the signature for the routes() method so that it returns a
 // http.Handler instead of *http.ServeMux.
@@ -16,6 +19,10 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	// Wrap the existing chain with the logRequest middleware.
-	return app.recoveryPanic(app.logRequest(secureHeaders(mux)))
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoveryPanic, app.logRequest, secureHeaders)
+
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
 }
