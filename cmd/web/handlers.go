2945d1a63d5aa2022562d600087f711bbfab0352
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-07-04 11:51:37 +0700

    custom function template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,10 +21,14 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the new render helper.
-	app.render(w, http.StatusOK, "home.tmpl.gohtml", &templateData{
-		Snippets: snippets,
-	})
+	// Call the newTemplateData() helper to get a templateData struct containing
+	// the 'default' data (which for now is just the current year), and add the
+	// snippets slice to it.
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	// Pass the data to the render() helper as normal.
+	app.render(w, http.StatusOK, "home.tmpl.gohtml", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -44,10 +48,12 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	// Use the new render helper.
-	app.render(w, http.StatusOK, "view.tmpl.gohtml", &templateData{
-		Snippet: snippet,
-	})
+
+	// And do the same thing again here...
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
+	app.render(w, http.StatusOK, "view.tmpl.gohtml", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,14 +4,26 @@ import (
 	"github.com/DaoVuDat/snippetbox/internal/models"
 	"html/template"
 	"path/filepath"
+	"time"
 )
 
-// Define a templateData type to act as the holding structure for
-// any dynamic data that we want to pass to our HTML templates.
-// At the moment it only contains one field, but we'll add more
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -30,24 +42,25 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 	// Loop through the page filepaths one-by-one.
 	for _, page := range pages {
-		// Extract the file name (like 'home.tmpl') from the full filepath
-		// and assign it to the name variable.
 		name := filepath.Base(page)
 
-		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.gohtml")
+		// The template.FuncMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.gohtml")
 		if err != nil {
 			return nil, err
 		}
 
 		// Call ParseGlob() *on this template set* to add any partials.
-		ts, err = template.ParseGlob("./ui/html/partials/*.gohtml")
+		ts, err = ts.ParseGlob("./ui/html/partials/*.gohtml")
 		if err != nil {
 			return nil, err
 		}
 
 		// Call ParseFiles() *on this template set* to add the page template
-		ts, err = template.ParseFiles(page)
+		ts, err = ts.ParseFiles(page)
 
 		if err != nil {
 			return nil, err
