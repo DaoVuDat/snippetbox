f514c5d30d197439fb344a55fb02eebd98584ff0
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-07-07 20:40:56 +0700

    add signup page with bcrypt

--- cmd/web/router.go
+++ cmd/web/router.go
@@ -26,15 +26,18 @@ func (app *application) routes() http.Handler {
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
-	// And then create the routes using the appropriate methods, patterns and
-	// handlers.
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
 	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
-	// Create a middleware chain containing our 'standard' middleware
-	// which will be used for every request our application receives.
+	// Add the five new routes, all of which use our 'dynamic' middleware chain.
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
 	standard := alice.New(app.recoveryPanic, app.logRequest, secureHeaders)
 
 	// Return the 'standard' middleware chain followed by the servemux.
