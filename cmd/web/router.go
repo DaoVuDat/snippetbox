2844fcb85c75e8bdf97f266d9d104448a464ddda
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-07-09 15:48:14 +0700

    using file embedded

--- cmd/web/router.go
+++ cmd/web/router.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/DaoVuDat/snippetbox/ui"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 	"net/http"
@@ -12,17 +13,22 @@ func (app *application) routes() http.Handler {
 	// Initialize the router.
 	router := httprouter.New()
 
-	// Create a handler function which wraps our notFound() helper, and then
-	// assign it as the custom handler for 404 Not Found responses. You can also
-	// set a custom handler for 405 Method Not Allowed responses by setting
-	// router.MethodNotAllowed in the same way too.
 	router.NotFound = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		app.notFound(writer)
 	})
 
-	// Update the pattern for the route for the static files.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	// Take the ui.Files embedded filesystem and convert it to a http.FS type so
+	// that it satisfies the http.FileSystem interface. We then pass that to the
+	// http.FileServer() function to create the file server handler.
+	fileServer := http.FileServer(http.FS(ui.Files))
+
+	// Our static files are contained in the "static" folder of the ui.Files
+	// embedded filesystem. So, for example, our CSS stylesheet is located at
+	// "static/css/main.css". This means that we now longer need to strip the
+	// prefix from the request URL -- any requests that start with /static/ can
+	// just be passed directly to the file server and the corresponding static
+	// file will be served (so long as it exists).
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	// Use the nosurf middleware on all our 'dynamic' routes.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
